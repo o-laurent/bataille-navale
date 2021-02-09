b611113e7932fbf9b3b741ee01798a200143693f
Laurent Olivier <olivier.laurent@ensta-paris.fr>
2021-02-09 10:54:37 +0100

    Add Board (exercice 1)

---
+++ src/main/java/ensta/Board.java
@@ -0,0 +1,52 @@
+package ensta;
+
+public class Board {
+    private String name;
+    private Character[][] shipGrid;
+    private Boolean[][] hitGrid;
+
+    public Board(String name, int gridSize) {
+        this.name = name;
+        this.shipGrid = new Character[gridSize][gridSize];
+        this.hitGrid = new Boolean[gridSize][gridSize];
+    }
+
+    public Board(String name) {
+        this.name = name;
+        this.shipGrid = new Character[10][10];
+        this.hitGrid = new Boolean[10][10];
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Character[][] getShip() {
+        return this.shipGrid;
+    }
+
+    public Boolean[][] getHit() {
+        return this.hitGrid;
+    }
+
+    public void print() {
+        System.out.println("Ship Grid");
+        for (Character[] x : getShip()) {
+            for (Character y : x) {
+                System.out.print(y + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Hit Grid");
+        for (Boolean[] x : getHit()) {
+            for (Boolean y : x) {
+                if (y) {
+                    System.out.print("x ");
+                } else {
+                    System.out.print(". ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/ensta/TestBoard.java
@@ -0,0 +1,8 @@
+package ensta;
+
+public class TestBoard {
+    public void main() {
+        Board testBoard = new Board("Test");
+        testBoard.print();
+    }
+}

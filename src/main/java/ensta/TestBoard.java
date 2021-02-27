93f77ed58a6eee085336ebe682f5f8bab01f6e18
Laurent Olivier <olivier.laurent@ensta-paris.fr>
2021-02-27 13:55:36 +0100

    Test Board for print

--- src/main/java/ensta/TestBoard.java
+++ src/main/java/ensta/TestBoard.java
@@ -1,15 +1,25 @@
 package ensta;
 
-import ensta.AbstractShip.AbstractShip;
+import ensta.AbstractShip.BattleShip;
 import ensta.AbstractShip.Carrier;
+import ensta.AbstractShip.Destroyer;
 import ensta.AbstractShip.Orientation;
 
 
 public class TestBoard {
-    public void main() {
+    public static void main(String[] args) {
         Board testBoard = new Board("Test");
-        Carrier carrier = new Carrier(EAST)
-        testBoard.putShip(carrier, 0, y);
+        Carrier carrier = new Carrier(Orientation.EAST);
+        BattleShip bShip = new BattleShip(Orientation.SOUTH);
+        Destroyer destr = new Destroyer(Orientation.SOUTH);
+        try {
+            testBoard.putShip(carrier, 1, 1);
+            testBoard.putShip(bShip, 2, 1);
+            testBoard.putShip(destr, 9, 1);
+        }
+        catch (Exception errException) {
+            System.out.println(errException);
+        }
         testBoard.print();
     }
 }
